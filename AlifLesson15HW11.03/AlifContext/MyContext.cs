ff7ee8ef2b631199fdb1771e462ca27a9727fb33
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-03-14 09:03:15 -0400

    Add project files.

---
+++ AlifLesson15HW11.03/AlifContext/MyContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AlifLesson15HW11._03.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlifLesson15HW11._03.AlifContext
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public MyContext()
+        {
+                
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder
+            )
+        {
+            optionsBuilder
+                .UseSqlServer("Data source = DESKTOP-SS5TGJO\\SQLEXPRESS; initial catalog = Person1; Integrated security = true;");
+        }
+
+        internal Task FindAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlifLesson15HW11.03/Program.cs
@@ -0,0 +1,112 @@
+﻿using AlifLesson15HW11._03.AlifContext;
+using AlifLesson15HW11._03.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace AlifLesson15HW11._03
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(@"1. Read"+
+                                    "2. Delete"+
+                                    "3. Create"+
+                                    "4. Update");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        await Read();
+                        break;
+                    case "2":
+                        await Delete();    
+                        break;
+                    case "3":
+                        await Create();
+                        break;
+                    case "4":
+                        await Update();
+                        break;
+                }
+
+            }
+        }
+        private static async Task Delete()
+        {
+            Console.Write("Id:");
+            var id = int.Parse(Console.ReadLine());
+            using (var alifCtx = new MyContext())
+            {
+                var persons = await alifCtx.Persons.FindAsync(id);
+                alifCtx.Remove(persons);
+                try
+                {
+                    await alifCtx.SaveChangesAsync();
+                }
+                catch (System.Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+        private static async Task Create()
+        {
+            var @newPerson = new Person();
+            
+            Console.WriteLine("LastName: ");
+            newPerson.LastName = Console.ReadLine();
+
+            Console.WriteLine("FirstName: ");
+            newPerson.FirstName = Console.ReadLine();
+
+            Console.WriteLine("MiddleName: ");
+            newPerson.MiddleName = Console.ReadLine();
+
+            Console.WriteLine("Date of birth: ");
+            newPerson.BirthDate = DateTime.Parse(Console.ReadLine());
+
+            using (var ctx = new MyContext())
+            {
+                ctx.Persons.Add(newPerson);
+                await ctx.SaveChangesAsync();
+            }
+        }
+        static async Task Read()
+        {
+            
+            using (var alifCtx = new MyContext())
+            {
+                var persons = alifCtx.Persons;
+                await persons.ForEachAsync(p => Console.WriteLine($"Id:{p.Id}\tFirstName:{p.FirstName}")); 
+            }
+        }
+        private static async Task Update()
+        {
+            Console.WriteLine("Id:");
+            var id = int.Parse(Console.ReadLine());
+            using (var alifCtx = new MyContext())
+            {
+                var persons = await alifCtx.Persons.FindAsync(id);
+                Console.WriteLine("LastName:");
+                persons.LastName = Console.ReadLine();
+
+                Console.WriteLine("FirstName:");
+                persons.FirstName = Console.ReadLine();
+
+                Console.WriteLine("MiddleName:");
+                persons.MiddleName = Console.ReadLine();
+
+                Console.WriteLine("Date of Birth:");
+                persons.BirthDate = DateTime.Parse( Console.ReadLine());
+
+                await alifCtx.SaveChangesAsync();
+            }
+
+        }
+    }
+}
